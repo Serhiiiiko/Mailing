5c74838f1c7b064f36adc496bb4c294e2aaf4dc3
Serhiiiiko <2021elit0049@ms.sumdu.edu.ua>
2024-03-19 12:25:46 +0200

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : Controller
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+            return Ok();
+        }
+
+    }
+}
---
+++ Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using WebApiApp.Models;
+
+namespace WebApiApp.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
